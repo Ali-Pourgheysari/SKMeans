52943833d2854c3b732b70cae363a6ffea0a93c1
Ali Pourgheysari <a.geysar@gmail.com>
2023-07-06 09:48:41 +0330

    Add save in file

--- Code.py
+++ Code.py
@@ -1,6 +1,6 @@
 import skmeans
 import os
-
+import json
 
 def get_txt_input(path):
     info_matrix = []
@@ -18,6 +18,7 @@ def get_txt_input(path):
                 
     return info_matrix, file_names
 
+
 def get_csv_input(path):
     info_matrix = []
     file_names = []
@@ -33,18 +34,7 @@ def get_csv_input(path):
     return info_matrix, file_names
 
 
-def main():
-
-    no_iters = 10
-
-    info_matrix, file_names = get_csv_input(path='embedding/output55.csv')
-    # info_matrix, file_names = get_txt_input(path='embedding/')
-
-    kmeans_inst = skmeans.SKMeans(no_clusters=len(info_matrix), iters=no_iters)
-    kmeans_inst.fit(info_matrix, two_pass=True)
-
-    labels = kmeans_inst.labels
-
+def organizing_clusters(labels, file_names):
     # create dictionary of clusters
     cluster_dict = {}
     for i in range(len(labels)):
@@ -61,6 +51,34 @@ def main():
     new_cluster_dict = {}
     for i, (key, value) in enumerate(cluster_dict.items()):
         new_cluster_dict[i] =  value
+
+    return new_cluster_dict
+
+
+def save_in_txt(path, cluster_dict):
+    # Convert dictionary to JSON string
+    json_str = json.dumps(cluster_dict)
+
+    # Write JSON string to file
+    with open(path, 'w') as file:
+        file.write(json_str)
+
+def main():
+
+    no_iters = 10
+    no_clusters = 50
+
+    info_matrix, file_names = get_csv_input(path='embedding/output55.csv')
+    # info_matrix, file_names = get_txt_input(path='embedding/')
+
+    kmeans_inst = skmeans.SKMeans(no_clusters=no_clusters, iters=no_iters)
+    kmeans_inst.fit(info_matrix, two_pass=True)
+
+    labels = kmeans_inst.labels
+
+    new_cluster_dict = organizing_clusters(labels, file_names)
+    
+    # save_in_txt(path='clustering_output.txt', cluster_dict=new_cluster_dict)
 
     for key in new_cluster_dict:
         print(key, new_cluster_dict[key])
