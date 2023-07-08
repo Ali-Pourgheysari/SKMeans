c3d028375c3a09c299db128ce6a039659faaec5a
Ali Pourgheysari <a.geysar@gmail.com>
2023-07-08 13:15:07 +0330

    Add load centers and read new embedding

--- Code.py
+++ Code.py
@@ -3,8 +3,9 @@ import os
 
 import skmeans
 
+
 #  Function to read input from txt files
-def get_txt_input(path):
+def load_txt_input(path):
     info_matrix = []
     file_names = []
 
@@ -22,8 +23,9 @@ def get_txt_input(path):
                 
     return info_matrix, file_names
 
+
 # Function to read input from csv files
-def get_csv_input(path):
+def load_csv_input(path):
     info_matrix = []
     file_names = []
     
@@ -38,6 +40,7 @@ def get_csv_input(path):
 
     return info_matrix, file_names
 
+
 # Function to organize clusters
 def organizing_clusters(labels, file_names):
     # create dictionary of clusters
@@ -49,25 +52,46 @@ def organizing_clusters(labels, file_names):
         else:
             cluster_dict[labels[i]].append(file_names[i][:-4])
 
-    # sort keys
-    cluster_dict = {k: v for k, v in sorted(cluster_dict.items(), key=lambda item: item[0])}
+    # # sort keys
+    # cluster_dict = {k: v for k, v in sorted(cluster_dict.items(), key=lambda item: item[0])}
     
-    # organize keys in a way that they start from 0 and go up to the number of clusters
-    new_cluster_dict = {}
-    for i, (key, value) in enumerate(cluster_dict.items()):
-        new_cluster_dict[i] =  value
+    # # organize keys in a way that they start from 0 and go up to the number of clusters
+    # new_cluster_dict = {}
+    # for i, (key, value) in enumerate(cluster_dict.items()):
+    #     new_cluster_dict[i] =  value
 
-    return new_cluster_dict
+    return cluster_dict
+
 
 # Function to save clusters in txt files
 # Eache cluster is saved in a separate txt file
-def save_in_txt(path, cluster_dict):
+def save_in_txt(cluster_path, center_path, cluster_dict, centers ,save_format):
 
     for cluster_key, cluster_val in cluster_dict.items():
-        # Convert dictionary to JSON string
-        json_str = json.dumps(cluster_val)
-        with open(f'{path}_{cluster_key}.txt', 'w') as file:
-            file.write(json_str)
+        with open(f'{cluster_path}_{cluster_key}.txt', save_format) as file:
+            for item in cluster_val:
+                file.write(f'{item}\n')
+    
+    # save centers separately
+    for i, center in enumerate(centers):
+        with open(f'{center_path}_{i}.txt', save_format) as file:
+            for item in center:
+                file.write(f'{item}\n')
+    
+
+def load_centers(path):
+    centers = []
+    for filename in os.listdir(path):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(path, filename)
+            with open(file_path, 'r') as file:
+                numbers = []
+                for line in file:
+                    number = float(line.strip())
+                    numbers.append(number)
+                centers.append(numbers)
+                
+    return centers
 
 
 def main():
@@ -75,21 +99,37 @@ def main():
     no_iters = 50
     no_clusters = 10
 
-    # info_matrix, file_names = get_csv_input(path='embedding/output55.csv')
-    info_matrix, file_names = get_txt_input(path='embedding_hoopad_staff/')
-
     # create an instance of SKMeans class
     kmeans_inst = skmeans.SKMeans(no_clusters=no_clusters, iters=no_iters)
+    
+    # if new_embedding is not empty, do not run the kmeans algorithm again
+    if not os.listdir('new_embedding/'):
 
-    # fit the model
-    kmeans_inst.fit(info_matrix, two_pass=True)
+        # info_matrix, file_names = load_csv_input(path='embedding/output55.csv')
+        info_matrix, file_names = load_txt_input(path='embedding_hoopad_staff/')
+
+        # fit the model
+        kmeans_inst.fit(info_matrix, two_pass=True)
+
+        save_format = 'w'
+
+    else:
+
+        info_matrix, file_names = load_txt_input(path='new_embedding/')
+        centers = load_centers(path='center_output/')
+        
+        kmeans_inst.set_centers(centers)
+        kmeans_inst.cluster_new_data(info_matrix)
+        kmeans_inst.fit(info_matrix)
+
+        save_format = 'a'
 
     labels = kmeans_inst.get_labels()
-    centres = kmeans_inst.get_centres()
+    centers = kmeans_inst.get_centers()
 
     new_cluster_dict = organizing_clusters(labels, file_names)
     
-    save_in_txt(path='cluster_output/cluster', cluster_dict=new_cluster_dict)
+    save_in_txt(cluster_path='cluster_output/cluster', center_path='center_output/center', cluster_dict=new_cluster_dict, centers=centers, save_format=save_format)
 
     for key in new_cluster_dict:
         print(key, new_cluster_dict[key])
